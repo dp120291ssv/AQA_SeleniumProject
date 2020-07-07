271ecd3b975b2ad0c76280b5a1b8005a6ac6d0fb
Semenov Sergei <dp120291ssv@gmail.com>
2020-07-07 10:49:38 +0300

    add VRT diff

--- src/main/java/common/CommonActions.java
+++ src/main/java/common/CommonActions.java
@@ -1,44 +1,23 @@
-//package common;
-//
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.chrome.ChromeDriver;
-//import java.util.concurrent.TimeUnit;
-//import static common.Config.BROWSER_NAME;
-//import static constants.Constants.TimeoutVariables.IMPLICIT_WAIT;
-//
-//public class CommonActions {
-//
-//    public WebDriver driver;
-//    public static CommonActions instance = null;
-//
-//    /**
-//     *Select and install the driver according to the selected version in the config
-//	 */
-//    public CommonActions() {
-//        switch (BROWSER_NAME) {
-//            case "chrome_mac":
-//                System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
-//                driver = new ChromeDriver();
-//                break;
-//			case "chrome_windows":
-//				System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-//				driver = new ChromeDriver();
-//				break;
-//			case "mozzila_mac":
-//				System.setProperty("webdriver.chrome.driver", "src/main/resources/geckodriver");
-//				driver = new ChromeDriver();
-//				break;
-//		}
-//        driver.manage().timeouts().implicitlyWait(IMPLICIT_WAIT, TimeUnit.SECONDS);
-//        driver.manage().window().maximize();
-//    }
-//
-//	/**
-//	 * static method to create instance of Singleton class
-//	 */
-//    public static CommonActions getInstance() {
-//        if (instance == null)
-//            instance = new CommonActions();
-//        return instance;
-//    }
-//}
+package common;
+
+import com.codeborne.selenide.Selenide;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import static common.Config.CLEAR_COOKIES;
+
+public class CommonActions {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(CommonActions.class);
+
+	public static void clearBrowserCookiesAndStorage() {
+		if(CLEAR_COOKIES) {
+			try {
+				Selenide.clearBrowserCookies();
+				Selenide.clearBrowserLocalStorage();
+				Selenide.executeJavaScript("window.sessionStorage.clear()");
+			} catch (Exception e) {
+				LOGGER.error("Fail to clearBrowserCookiesAndStorage()", e.getMessage());
+			}
+		}
+	}
+}
